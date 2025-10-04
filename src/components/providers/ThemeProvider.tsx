8fa3bf84b1c042c13ea291d88b1c4e98904bc187
DevBielCastro <gabriel.castrogt10@gmail.com>
2025-10-04 15:07:22 -0300

    atualização e correção do themeprovider.tsx

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+// A correção está nesta linha: importamos o tipo diretamente de "next-themes"
+import { type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  // A correção principal está aqui: attribute="class"
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>
       {children}
