4874227c3d10fc65b28faf87f9f935ac36f10149
Gabriel Castro <93748166+DevBielCastro@users.noreply.github.com>
2025-10-13 16:37:25 -0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,6 @@
-import { FaReact, FaNodeJs, FaDocker, FaGitAlt, FaHtml5, FaCss3Alt, FaPython, FaAws, FaLinux } from "react-icons/fa";
-import { SiPostgresql, SiTypescript, SiExpress, SiNextdotjs, SiTailwindcss, SiMongodb, SiVercel, SiVsco, SiGithubactions, SiMysql, SiNginx, SiPm2, SiJest } from "react-icons/si";
+import { FaReact, FaNodeJs, FaDocker, FaGitAlt, FaHtml5, FaCss3Alt, FaUsers, FaCode } from "react-icons/fa";
+import { SiPostgresql, SiTypescript, SiExpress, SiNextdotjs, SiTailwindcss, SiMongodb, SiVercel, SiGithubactions, SiMysql, SiNginx, SiPm2, SiJest, SiVscode } from "react-icons/si";
+import { TbApi, TbShieldLock } from "react-icons/tb";
 import { SectionWrapper } from "./SectionWrapper";
 
 export function Skills() {
@@ -19,9 +20,9 @@ export function Skills() {
       category: "Back-end", 
       technologies: [ 
         { icon: <FaNodeJs size={24} />, name: "Node.js" },
-        { icon: <SiExpress size={24} />, name: "Express.js" },
-        { icon: <FaPython size={24} />, name: "Python & Django" },
-        { icon: <SiTypescript size={24} />, name: "APIs REST & JWT" }
+        { icon: <SiExpress size={24} />, name: "Express" },
+        { icon: <TbApi size={24} />, name: "APIs REST" },
+        { icon: <TbShieldLock size={24} />, name: "JWT" }
       ] 
     },
     { 
@@ -36,20 +37,20 @@ export function Skills() {
       category: "DevOps & Ferramentas", 
       technologies: [ 
         { icon: <FaDocker size={24} />, name: "Docker" },
-        { icon: <SiGithubactions size={24} />, name: "CI/CD" },
         { icon: <FaGitAlt size={24} />, name: "Git & GitHub" },
+        { icon: <SiGithubactions size={24} />, name: "CI/CD (Actions)" },
+        { icon: <SiVercel size={24} />, name: "Vercel" },
         { icon: <SiNginx size={24} />, name: "Nginx" },
         { icon: <SiPm2 size={24} />, name: "PM2" },
-        { icon: <SiVercel size={24} />, name: "Vercel" }
+        { icon: <SiVscode size={24} />, name: "VS Code" }
       ] 
     },
     { 
       category: "Metodologias & Testes", 
       technologies: [ 
-        { icon: <FaAws size={24} />, name: "Agile (Scrum)" },
-        { icon: <FaLinux size={24} />, name: "Clean Code/S.O.L.I.D." },
-        { icon: <SiJest size={24} />, name: "Jest & Supertest" },
-        { icon: <SiVsco size={24} />, name: "VS Code" }
+        { icon: <FaUsers size={24} />, name: "Agile (Scrum)" },
+        { icon: <FaCode size={24} />, name: "Clean Code/S.O.L.I.D." },
+        { icon: <SiJest size={24} />, name: "Jest & Supertest" }
       ] 
     },
   ];
